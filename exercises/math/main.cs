792baf1dd8d196435e363026f8f383fa6ee2c78c
MyBang <my25dec@gmail.com>
2024-02-20 15:53:31 +0100

    math exercise

--- exercises/math/main.cs
+++ exercises/math/main.cs
@@ -1,13 +1,32 @@
 using System;
-using System.numerics;
+using System.Numerics;
 using static System.Console;
 
 class math{
-	double sqrt = Math.Sqrt(2.0);
-	Write($"sqrt2^2 = {sqrt2*sqrt2} (should equal 2)\n");
+	//small tasks
+	static void main(){
+		double sqrt = Math.Sqrt(2.0);
+		Write($"sqrt2^2 = {sqrt2*sqrt2} (should equal 2)\n");
 
-	double power2 = Math.Pow(2.0, 1d/5d);
-	Write($"2^1/5 = {power2} with (2^1/5)^5 = {Math.Pow(power2, 5.0)}\n");
+		double power2 = Math.Pow(2.0, 1d/5d);
+		Write($"2^1/5 = {power2} with (2^1/5)^5 = {Math.Pow(power2, 5.0)}\n");
 
-}
+		double exp = Math.Exp(PI);
+		Write($"exp(Pi) = {exp} with Math.Log(Exp(PI)) = {PI} \n");
+
+		double epi = Math.Pow(Math.PI, Math.E);
+		Write($"pi^e = {e} with ln(pi^e) = {Math.Log(epi, Math.PI)} \n");
+		}//main
+
+	//the gamma function
+	static int Main(){
+		double prod=1;
+		for(double x=1;x<10;x+=1){
+		Write($"fgamma({x})={sfunc.fgamma(x)} \t {x-1}!={prod}\n");
+		prod*=x;
+		}
+		return 0;
+		}
+
+}//math
 
