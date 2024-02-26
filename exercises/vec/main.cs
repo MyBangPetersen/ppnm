b1df1b229c9224354122eb7100c3e16f14d2bd81
MyBang <my25dec@gmail.com>
2024-02-26 16:22:26 +0100

    working on vector

---
+++ exercises/vec/main.cs
@@ -0,0 +1,13 @@
+using static System.Console;
+using static System.Math;
+
+public class vec(){
+	public double x,y,z;
+
+	//constructors
+	public vec(){x=y=z=0;}
+	public vec(double x,double y,double z){ this.x=x; this.y=y; this.z=z;}
+
+
+
+}//vec
