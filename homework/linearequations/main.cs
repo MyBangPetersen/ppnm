32595277f23a7ccbef6ba2dc8d0ac199e3358b07
MyBang <my25dec@gmail.com>
2024-03-28 15:14:05 +0100

    working on lineq

--- homework/linearequations/main.cs
+++ homework/linearequations/main.cs
@@ -1,12 +1,90 @@
+//generating a random matrix of some size
+public static class RM{
+	public static matrix randommatrix(int rowsize,int colsize){
+		matrix emptm = new matrix(rowsize, colsize);
+
+		var rnd = new System.Random(1);
+
+		for (int i = 0; i<rowsize; i++){
+			for (int j = 0; j<colsize; j++){
+			emptm[i,j] = rnd.NextDouble();
+			}}
+
+		return emptm;
+		}
+	}
+
+
+//generating a vector of some size
+public static class RV{
+	public static vector randomvector(int indexsize){
+		vector emptv = new vector(indexsize);
+
+		var rnd = new System.Random(1);
+
+		for (int n = 0; n<indexsize; n++){
+		emptv[n] = rnd.NextDouble();
+		}
+
+		return emptv;
+		}
+	}
+
+public static class QRGS{
+	//gram smith orthoginalization
+	public static (matrix, matrix) decomp(matrix A){
+		matrix Q = A.copy();
+		matrix R = new matrix(A.size2, A.size2);
+		for(int i = 0; i<A.size2; i++){
+			R[i,i] = Q[i].norm();
+			Q[i]/= R[i,i];
+			for(int j = i + 1; j<A.size2 ; j++){R[i, j] = Q[i].dot(Q[j]);
+			Q[j]-= Q[i] * R[i,j];
+			}}
+			return (Q, R);
+		}
+
+	//solving equation
+	public static vector solve(matrix Q, matrix R, vector b){
+		vector b = b.copy();
+				}
+
+	//public static double det(matrix R){ ... }
+
+	//public static matrix inverse(matrix Q,matrix R){ ... }
+
+
+	}//QRGS
+
+
 class main{
 static void Main(){
-	vector ve;
-	ve=new vector(n:1); ve.print("ve=");
-	ve=new vector(5,3,9); ve.print("ve=");
-	ve=new vector(7,8,9,8,7); ve.print("ve=");
-	var ma=new matrix("1 2 ; 5 6");
-	ma.print();
-	(ma+ma.T).print();
-	(matrix.id(2)).print();
-}
-}
+	System.Console.Write("Making a random matrix");
+	matrix A = RM.randommatrix(3,3);
+	A.print();
+
+	System.Console.Write("Making a random vector \n");
+	vector H = RV.randomvector(3);
+	H.print();
+
+	//decomp testing
+	System.Console.Write("decomp works");
+	(matrix Q, matrix R) = QRGS.decomp(A);
+	Q.print();
+	matrix C = Q*R;
+	C.print();
+	System.Console.Write("R is upper tringular");
+	R.print();
+	System.Console.Write("Q^T*Q=1");
+	matrix QTQ = Q.T*Q;
+	QTQ.print();
+
+	//solve testing
+	
+	}//Main
+	}//main
+
+
+
+
+
