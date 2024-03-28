0451549f35bf4b98080a4d2b4d291f784fd4a569
MyBang <my25dec@gmail.com>
2024-03-28 16:33:04 +0100

    lineq

--- homework/linearequations/main.cs
+++ homework/linearequations/main.cs
@@ -46,8 +46,21 @@ public static class QRGS{
 
 	//solving equation
 	public static vector solve(matrix Q, matrix R, vector b){
-		vector b = b.copy();
-				}
+		int length = b.size;
+		vector a = new vector(length);
+		matrix QT = Q.T;
+		vector QTb = QT*b;
+		//solving the lineq from the bottom since R is upper triangular
+		for (int i = length - 1; i>=0; i--){
+			double sum = 0;
+			for (int j=i; j<length; j++){
+				sum += R[i,j] * a[j];
+			}
+
+			a[i] = (QTb[i] - sum)/R[i, i];
+		}
+		return a;
+		}
 
 	//public static double det(matrix R){ ... }
 
@@ -59,15 +72,17 @@ public static class QRGS{
 
 class main{
 static void Main(){
+	/*
 	System.Console.Write("Making a random matrix");
-	matrix A = RM.randommatrix(3,3);
+	matrix A = RM.randommatrix(5,3);
 	A.print();
 
 	System.Console.Write("Making a random vector \n");
-	vector H = RV.randomvector(3);
+	vector H = RV.randomvector(4);
 	H.print();
 
 	//decomp testing
+
 	System.Console.Write("decomp works");
 	(matrix Q, matrix R) = QRGS.decomp(A);
 	Q.print();
@@ -77,14 +92,24 @@ static void Main(){
 	R.print();
 	System.Console.Write("Q^T*Q=1");
 	matrix QTQ = Q.T*Q;
-	QTQ.print();
+	QTQ.print(); */
 
 	//solve testing
-	
+	System.Console.Write("Making a vector and square matrix of same size");
+	matrix A = RM.randommatrix(3,3);
+        A.print();
+	System.Console.Write("\n");
+        vector H = RV.randomvector(3);
+	H.print();
+	System.Console.Write("Decomposing using decomp from before");
+	(matrix Q, matrix R) = QRGS.decomp(A);
+	Q.print();
+	System.Console.Write("The solution to the system \n");
+	vector a = QRGS.solve(Q, R, H);
+	a.print();
+	System.Console.Write("Ax=b \n");
+	vector D = A*a;
+	D.print();
+
 	}//Main
 	}//main
-
-
-
-
-
